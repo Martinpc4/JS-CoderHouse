2a94304864b7bad6f04790c685a4dbe1f763b71b
Martín Pérez Cobo <martinperezcobo4@outlook.com>
2021-06-16 02:59:42 -0300

    New Done Feature!
    - Added a new color to the "color.scss" file called "done".
    - Coded the done state color for the tasks, reminders and goals.

--- js/classes.js
+++ js/classes.js
@@ -131,27 +131,27 @@ class task {
     generateDOM(display) {
         $("#tasksCtr").prepend(`
             <div id=${this.id} class="task" ${display === true ? 'style="display: none;"' : ""}>
-                <div class="task__bar background-${userData.userLastLocation.projectColor}"></div>
+                <div class="task__bar background-${userData.userLastLocation.projectColor} ${this.doneState === true ? "background-done" : ""}"></div>
                 <div class="task__ctr">
-                    <p class="task__title">${this.name}</p>
-                    <p class="task__dueDate">
+                    <p class="task__title ${this.doneState === true ? "font-done" : ""}" >${this.name}</p>
+                    <p class="task__dueDate ${this.doneState === true ? "font-done" : ""}">
                         ${this.dueDate === false ? "" : String(this.dueDate.getDate()) + `/` + String(this.dueDate.getMonth() + 1) + `/` + String(this.dueDate.getFullYear())}
                     </p>
-                    <p class="task__description">${this.description}</p>
+                    <p class="task__description ${this.doneState === true ? "font-done" : ""}">${this.description}</p>
                     <div class="task__actions">
                         <div class="task__actions-complete">
                             ${this.doneState === false ? `
-                            <svg class="btnTaskComplete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                            <svg class="btnTaskComplete ${this.doneState === true ? "fill-done" : ""}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"/>
                             </svg>
                             ` : `
-                            <svg class="btnTaskComplete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                            <svg class="btnTaskComplete ${this.doneState === true ? "fill-done" : ""}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                 <path d="M17.28 9.28a.75.75 0 00-1.06-1.06l-5.97 5.97-2.47-2.47a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l6.5-6.5z"/><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"/>
                             </svg>
                             `}
                         </div>
                         <div class="task__actions-delete">
-                            <svg class="btnTaskDelete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                            <svg class="btnTaskDelete ${this.doneState === true ? "fill-done" : ""}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                 <path fill-rule="evenodd" d="M16 1.75V3h5.25a.75.75 0 010 1.5H2.75a.75.75 0 010-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75zm-6.5 0a.25.25 0 01.25-.25h4.5a.25.25 0 01.25.25V3h-5V1.75z"/><path d="M4.997 6.178a.75.75 0 10-1.493.144L4.916 20.92a1.75 1.75 0 001.742 1.58h10.684a1.75 1.75 0 001.742-1.581l1.413-14.597a.75.75 0 00-1.494-.144l-1.412 14.596a.25.25 0 01-.249.226H6.658a.25.25 0 01-.249-.226L4.997 6.178z"/><path d="M9.206 7.501a.75.75 0 01.793.705l.5 8.5A.75.75 0 119 16.794l-.5-8.5a.75.75 0 01.705-.793zm6.293.793A.75.75 0 1014 8.206l-.5 8.5a.75.75 0 001.498.088l.5-8.5z"/>
                             </svg>
                         </div>
@@ -190,14 +190,14 @@ class goal {
                             <path fill-rule="evenodd" d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"/>
                         </svg>
                     ` : `
-                        <svg class="btnGoalComplete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                        <svg class="btnGoalComplete fill-done" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                             <path d="M17.28 9.28a.75.75 0 00-1.06-1.06l-5.97 5.97-2.47-2.47a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l6.5-6.5z"/><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"/>
                         </svg>
                     `}
                 </div>
-                <p class="goal__title">${this.name}</p>
+                <p class="goal__title ${this.doneState === true ? "font-done" : ""}">${this.name}</p>
                 <div class="goal__actions-delete">
-                    <svg class="btnGoalDelete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                    <svg class="btnGoalDelete ${this.doneState === true ? "fill-done" : ""}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M16 1.75V3h5.25a.75.75 0 010 1.5H2.75a.75.75 0 010-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75zm-6.5 0a.25.25 0 01.25-.25h4.5a.25.25 0 01.25.25V3h-5V1.75z"/><path d="M4.997 6.178a.75.75 0 10-1.493.144L4.916 20.92a1.75 1.75 0 001.742 1.58h10.684a1.75 1.75 0 001.742-1.581l1.413-14.597a.75.75 0 00-1.494-.144l-1.412 14.596a.25.25 0 01-.249.226H6.658a.25.25 0 01-.249-.226L4.997 6.178z"/><path d="M9.206 7.501a.75.75 0 01.793.705l.5 8.5A.75.75 0 119 16.794l-.5-8.5a.75.75 0 01.705-.793zm6.293.793A.75.75 0 1014 8.206l-.5 8.5a.75.75 0 001.498.088l.5-8.5z"/>
                     </svg>
                 </div>
@@ -243,16 +243,16 @@ class reminder {
                             <path fill-rule="evenodd" d="M12 2.5a9.5 9.5 0 100 19 9.5 9.5 0 000-19zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12z"/>
                         </svg>
                     ` : `
-                        <svg class="btnReminderComplete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                        <svg class="btnReminderComplete fill-done" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                             <path d="M17.28 9.28a.75.75 0 00-1.06-1.06l-5.97 5.97-2.47-2.47a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l6.5-6.5z"/><path fill-rule="evenodd" d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0z"/>
                         </svg>
                     `}
                 </div>
-                ${this.onTime === true ? `<div class="reminder__status reminder__status--onTime"></div>` : `<div class="reminder__status reminder__status--overdue"></div>`}
-                <p class="reminder__title">${this.name}</p>
-                <p class="reminder__dueDate">${this.dueDate === false ? "" : String(this.dueDate.getDate()) + "/" + String(this.dueDate.getMonth() + 1) + "/" + String(this.dueDate.getFullYear())}</p>
+                ${this.onTime === true ? `<div class="reminder__status reminder__status--onTime ${this.doneState === true ? "background-done" : ""}"></div>` : `<div class="reminder__status reminder__status--overdue ${this.doneState === true ? "background-done" : ""}"></div>`}
+                <p class="reminder__title ${this.doneState === true ? "font-done" : ""}">${this.name}</p>
+                <p class="reminder__dueDate ${this.doneState === true ? "font-done" : ""}">${this.dueDate === false ? "" : String(this.dueDate.getDate()) + "/" + String(this.dueDate.getMonth() + 1) + "/" + String(this.dueDate.getFullYear())}</p>
                 <div class="reminder__actions-delete">
-                    <svg class="btnReminderDelete" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                    <svg class="btnReminderDelete ${this.doneState === true ? "fill-done" : ""}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                         <path fill-rule="evenodd" d="M16 1.75V3h5.25a.75.75 0 010 1.5H2.75a.75.75 0 010-1.5H8V1.75C8 .784 8.784 0 9.75 0h4.5C15.216 0 16 .784 16 1.75zm-6.5 0a.25.25 0 01.25-.25h4.5a.25.25 0 01.25.25V3h-5V1.75z"/><path d="M4.997 6.178a.75.75 0 10-1.493.144L4.916 20.92a1.75 1.75 0 001.742 1.58h10.684a1.75 1.75 0 001.742-1.581l1.413-14.597a.75.75 0 00-1.494-.144l-1.412 14.596a.25.25 0 01-.249.226H6.658a.25.25 0 01-.249-.226L4.997 6.178z"/><path d="M9.206 7.501a.75.75 0 01.793.705l.5 8.5A.75.75 0 119 16.794l-.5-8.5a.75.75 0 01.705-.793zm6.293.793A.75.75 0 1014 8.206l-.5 8.5a.75.75 0 001.498.088l.5-8.5z"/>
                     </svg>
                 </div>
